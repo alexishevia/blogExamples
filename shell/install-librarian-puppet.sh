c8486022bf7f67d0192e8c0703d468ccd986985c
Alexis Hevia <hevia.alexis@gmail.com>
2014-07-10 11:38:43 -0500

    add git installation step to install-librarian-puppet.sh

--- shell/install-librarian-puppet.sh
+++ shell/install-librarian-puppet.sh
@@ -6,6 +6,30 @@ PUPPET_DIR=/etc/puppet/
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
 
+$(which git > /dev/null 2>&1)
+FOUND_GIT=$?
+if [ "$FOUND_GIT" -ne '0' ]; then
+  echo 'Attempting to install git.'
+  $(which apt-get > /dev/null 2>&1)
+  FOUND_APT=$?
+  $(which yum > /dev/null 2>&1)
+  FOUND_YUM=$?
+
+  if [ "${FOUND_YUM}" -eq '0' ]; then
+    yum -q -y makecache
+    yum -q -y install git
+    echo 'git installed.'
+  elif [ "${FOUND_APT}" -eq '0' ]; then
+    apt-get -q -y update
+    apt-get -q -y install git
+    echo 'git installed.'
+  else
+    echo 'No package installer available. You may need to install git manually.'
+  fi
+else
+  echo 'git found.'
+fi
+
 $(which librarian-puppet > /dev/null 2>&1)
 FOUND_LIBRARIAN_PUPPET=$?
 if [ "$FOUND_LIBRARIAN_PUPPET" -ne '0' ]; then
