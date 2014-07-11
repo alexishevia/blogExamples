7d2b78276b89579bcb024847c7ab609f19335a41
Alexis Hevia <hevia.alexis@gmail.com>
2014-07-11 09:46:56 -0500

    use nodejs from apt-get

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,8 +1,5 @@
-$node_binaries_path = '/usr/local/node/node-default/bin'
-
 Exec {
-  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/local/bin',
-          'language-pack-UTF-8']
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/local/bin']
 }
 
 # --- Preinstall Stage ---#
@@ -14,7 +11,7 @@ stage { 'preinstall':
 # Define the install_packages class
 class install_packages {
   package { ['curl', 'build-essential', 'libfontconfig1', 'python',
-             'g++', 'make', 'wget', 'tar', 'mc', 'htop']:
+             'nodejs', 'npm',  'g++', 'make', 'wget', 'tar', 'mc', 'htop']:
     ensure => present
   }
 }
@@ -24,17 +21,19 @@ class { 'install_packages':
   stage => preinstall
 }
 
+# Setup your locale to avoid warnings
+file { '/etc/default/locale':
+  content => "LANG=\"en_US.UTF-8\"\nLC_ALL=\"en_US.UTF-8\"\n"
+}
+
 # --- NodeJS --- #
 
-class { 'nodejs':
-  version => 'v0.10.29'
-}
-
+# Because of a package name collision, 'node' is called 'nodejs' in Ubuntu.
+# Here we're adding a symlink so 'node' points to 'nodejs'
 file { '/usr/bin/node':
   ensure => 'link',
-  target => "${node_binaries_path}/node",
+  target => "/usr/bin/nodejs",
 }
-
 
 # --- MySQL --- #
 
