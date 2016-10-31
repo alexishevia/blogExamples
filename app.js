f60db7f4df7da6343ef9b0e33b08e2a073585687
Alexis Hevia <hevia.alexis@gmail.com>
2016-10-31 10:05:49 -0500

    add a db connection to express

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var massive = require("massive");
 
 var app = express();
+
+app.set('db', massive.connectSync({
+  connectionString : process.env.DATABASE_URL
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
